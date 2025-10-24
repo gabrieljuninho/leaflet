577833c5d3d508a8e7b849aa33329b04ce87436b
gabrieljuninho <gabrieltobing789@gmail.com>
2025-10-24 12:42:06 +0700

    chore: update project dependencies and add linting/prettier scripts

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./globals.css";
+
+import HomePage from "./pages/home";
+
+createRoot(document.getElementById("root")!).render(<HomePage />);
